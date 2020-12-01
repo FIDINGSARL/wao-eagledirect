b45e6191c046cf9f8ef587391f593ea459c0ec41
jwambeek <70319876+jwambeek@users.noreply.github.com>
2020-12-01 11:28:26 +0530

    update

--- modules/quote_calculator/__manifest__.py
+++ modules/quote_calculator/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'Quote Calculator',
-    'version': '1.1',
+    'version': '1.2',
     'Summary': 'Quote Calculations for custom Sales and Purchase Modules',
     'description': 'To add new fields in the existing modules',
     'license': 'LGPL-3',
